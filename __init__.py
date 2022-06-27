ec8bdef2e86ddeaa027227a1658360aaabe68407
huotong <824011142@qq.com>
2022-06-27 13:45:25 +0800

    ci($ai_model): version 0.0.2 release

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@ bl_info = {
     "author": "",
     "description": "",
     "blender": (3, 1, 0),
-    "version": (0, 0, 1),
+    "version": (0, 0, 2),
     "location": "",
     "warning": "",
     "category": "Generic"
